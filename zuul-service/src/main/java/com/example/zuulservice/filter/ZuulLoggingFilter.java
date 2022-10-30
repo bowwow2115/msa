d519c45bc36c886dbf2df0b6c0d6b08e19e979c7
bowwow2115 <bowwow2115@gmail.com>
2022-10-30 18:31:34 +0900

    리셋

--- zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
+++ zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
@@ -13,29 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class ZuulLoggingFilter extends ZuulFilter {
 
-    @Override
-    public Object run() throws ZuulException {
-        log.info("**************** printing logs: ");
 
-        RequestContext ctx = RequestContext.getCurrentContext();
-        HttpServletRequest request = ctx.getRequest();
-        log.info("**************** " + request.getRequestURI());
 
-        return null;
-    }
-
-    @Override
-    public String filterType() {
-        return "pre";
-    }
-
-    @Override
-    public int filterOrder() {
-        return 1;
-    }
-
-    @Override
-    public boolean shouldFilter() {
-        return true;
-    }
 }
