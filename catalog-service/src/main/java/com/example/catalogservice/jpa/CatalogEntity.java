99fb2ef9e607a373478415a672d3e569575d493b
bowwow2115 <bowwow2115@gmail.com>
2022-11-14 00:51:58 +0900

    getAllCatalog 서비스 로직 생성

--- catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogEntity.java
+++ catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogEntity.java
@@ -11,22 +11,18 @@ import java.util.Date;
 @Entity
 @Table(name = "catalog")
 public class CatalogEntity implements Serializable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 120, unique = true)
     private String productId;
-
     @Column(nullable = false)
     private String productName;
-
     @Column(nullable = false)
     private Integer stock;
-
     @Column(nullable = false)
-    private Integer uniPrice;
+    private Integer unitPrice;
 
     @Column(nullable = false, updatable = false, insertable = false)
     @ColumnDefault(value = "CURRENT_TIMESTAMP")
--- catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
+++ catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ReponseCatalog {
+public class ResponseCatalog {
     private String productId;
     private String productName;
     private Integer unitPrice;
